257db0ef98948dd5cd7f9f9a7ea9729f8f0c0e17
Nik Bornemeier <NikolausRB@gmail.com>
2018-09-11 15:28:40 -0600

    catch up commit

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ class App extends Component {
         <p>Most dogs benefit enormously from daily aerobic exercise (exercise that makes them pant, like fetch, tug, running and swimming), as well as at least one half-hour walk. I will help you choose activities that suit your dog’s individual personality and natural interests. Every dog has different needs.</p>
         <button>Schedule Dog Walk</button>
        </div>
-        <div className='happy-dog'>
-        </div>
+        {/* <div className='happy-dog'>
+        </div> */}
        <TabBar />
       </div>
     );
--- src/Components/TabBar/TabBar.js
+++ src/Components/TabBar/TabBar.js
@@ -4,23 +4,17 @@ import './TabBar.css';
 class TabBar extends Component {
   constructor() {
     super();
+
   }
 
   render() {
     return(
-      <div className="TabBar">
-        <div className="tab tab-one">
-        X
-        </div>     
-        <div className="tab tab-two">
-        X
-        </div>
-        <div className="tab tab-three">
-        X
-        </div>
-        <div className="tab tab-four">
-        X
-        </div>
+      <div className="navbar">
+        <a href="#home" className="active">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#contactz">ContactZ</a>
+
       </div>
     )
   }
